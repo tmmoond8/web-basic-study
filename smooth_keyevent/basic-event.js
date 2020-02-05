0699dd4e92f9e5d94f0b21ac74ded42d11cda036
TaeMin Moon <tmmoond8@gmail.com>
2020-02-05 09:59:53 +0900

    add smooth event toggle

--- smooth_keyevent/basic-event.js
+++ smooth_keyevent/basic-event.js
@@ -1,18 +1,22 @@
-const iconEl = document.querySelector('#icon-basic');
-document.addEventListener('keydown', (e) => {
-  if (!e.code.includes('Arrow')) {
-    return;
+(
+  () => {
+    const iconEl = document.querySelector('#icon-basic');
+    document.addEventListener('keydown', (e) => {
+      if (!e.code.includes('Arrow')) {
+        return;
+      }
+      if (e.code.includes('Up')) {
+        iconEl.style.top = `${parseInt(iconEl.style.top, 10) - 20}px`;
+      }
+      if (e.code.includes('Down')) {
+        iconEl.style.top = `${parseInt(iconEl.style.top, 10) + 20}px`;
+      }
+      if (e.code.includes('Left')) {
+        iconEl.style.left = `${parseInt(iconEl.style.left, 10) - 20}px`;
+      }
+      if (e.code.includes('Right')) {
+        iconEl.style.left = `${parseInt(iconEl.style.left, 10) + 20}px`;
+      }
+    })
   }
-  if (e.code.includes('Up')) {
-    iconEl.style.top = `${iconEl.offsetTop - 20}px`;
-  }
-  if (e.code.includes('Down')) {
-    iconEl.style.top = `${iconEl.offsetTop + 20}px`;
-  }
-  if (e.code.includes('Left')) {
-    iconEl.style.left = `${iconEl.offsetLeft - 20}px`;
-  }
-  if (e.code.includes('Right')) {
-    iconEl.style.left = `${iconEl.offsetLeft + 20}px`;
-  }
-})
+)();
