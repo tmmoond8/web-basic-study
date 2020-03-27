d6f61628851b2866ba4f9459bbb86b3f19f1e890
taemin <tmmoond8@gmail.com>
2020-03-27 21:53:32 +0900

    webpack study - html-webpack-plugin  모듈 사용하기

---
+++ webpack-study/plugin/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div>
+      <h3>플러그인을 공부합시다.</h3>
+      <p>다양한 플러그인을 사용하고, 유용성을 느껴봅시다.</p>
+      <u>ab</u>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ webpack-study/plugin/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './dist/index.html',
+    })
+  ],
+  mode: 'production'
+};
