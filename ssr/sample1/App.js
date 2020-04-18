d2025722f3c4d796d82d7c6b6cc7315fad34a2a1
taemin <tmmoond8@gmail.com>
2020-04-18 18:20:17 +0900

    ssr test 클라이언트에서 렌더링 하기

---
+++ ssr/sample1/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Home from './src/Home';
+import About from './src/About';
+
+class App extends React.Component {
+  state = {
+    page: this.props.page,
+  };
+
+  componentDidMount() {
+    window.onpopstate = event => {
+      this.setState({ page: event.state });
+    };
+  }
+
+  onChangePage = e => {
+    const page = e.target.dataset.page;
+    window.history.pushState(page, '', `/${page}`);
+    this.setState({ page });
+  };
+
+  render() {
+    const { page } = this.state;
+    const PageComponent = page === 'home' ? Home : About;
+    return (
+      <div className="container">
+        <button data-page="home" onClick={this.onChangePage}>
+          Home
+        </button>
+        <button data-page="about" onClick={this.onChangePage}>
+          About
+        </button>
+        <PageComponent />
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ ssr/sample1/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './template/index.html',
+    }),
+  ],
+  mode: 'production',
+};
