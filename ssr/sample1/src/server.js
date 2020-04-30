1d22512d32e06c067d5649c72ed1fcd0985b5dc1
taemin <tmmoond8@gmail.com>
2020-04-30 12:05:19 +0900

    study: 서버사이드 렌더링 - rederToNodeStream

--- ssr/sample1/src/server.js
+++ ssr/sample1/src/server.js
@@ -2,8 +2,12 @@ import express from 'express';
 import fs from 'fs';
 import path from 'path';
 import url from 'url';
-import { renderPage, prerenderPages } from './common';
+import { prerenderPages } from './common';
 import LruCache from 'lru-cache';
+import { ServerStyleSheet } from 'styled-components';
+import React from 'react';
+import App from './App';
+import { renderToNodeStream } from 'react-dom/server';
 
 const ssrCache = new LruCache({
   max: 100,
@@ -20,6 +24,10 @@ for (const page of prerenderPages) {
   prerenderHtml[page] = pageHtml;
 }
 
+const html = fs
+  .readFileSync(path.resolve(__dirname, '../dist/index.html'), 'utf8')
+  .replace('__STYLE_FROM_SERVER__', '');
+
 app.use('/dist', express.static('dist'));
 app.get('/favicon.ico', (req, res) => res.sendStatus(204));
 app.get('*', (req, res) => {
@@ -33,14 +41,25 @@ app.get('*', (req, res) => {
   const page = parseURL.pathname ? parseURL.pathname.substr(1) : 'home';
   const initialData = { page };
 
-  const pageHtml = prerenderPages.includes(page)
-    ? prerenderHtml[page]
-    : renderPage(page);
+  const isPrerender = prerenderPages.includes(page);
+  const result = (isPrerender ? prerenderHtml[page] : html).replace('__DATA_FROM_SERVER__', JSON.stringify(initialData),);
 
-  const result = pageHtml
-    .replace('__DATA_FROM_SERVER__', JSON.stringify(initialData))
-  ssrCache.set(cacheKey, result);
-  res.send(result);
+  if (isPrerender) {
+    ssrCache.set(cacheKey, result);
+    res.send(result);
+  } else {
+    const ROOT_TEXT = '<div id="root">';
+    const prefix = result.substr(0, result.indexOf(ROOT_TEXT) + ROOT_TEXT.length,);
+    const postfix = result.substr(prefix.length);
+    res.write(prefix);
+    const sheet = new ServerStyleSheet();
+    const reactElement = sheet.collectStyles(<App page={page} />);
+    const renderStream = sheet.interleaveWithNodeStream(renderToNodeStream(reactElement),);
+    renderStream.pipe(res, { end: false });
+    renderStream.on('end', () => {
+      res.end(postfix);
+    });
+  }
 });
 
 
