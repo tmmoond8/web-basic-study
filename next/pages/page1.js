b2b87b3ad5453592e316eb535f4ca1d6221bdd69
taemin <tmmoond8@gmail.com>
2020-05-01 16:35:27 +0900

    study: next - filep-loader 추가

--- next/pages/page1.js
+++ next/pages/page1.js
@@ -1,10 +1,11 @@
 import Head from 'next/head';
+import kangaroo from '../static/kangaroo-c.png';
 
 export default function Page1() {
   return (
     <div>
       <p>This is home page</p>
-      <img src="/static/kangaroo-c.png"/>
+      <img src={kangaroo}/>
       <Head>
         <title>page1</title>
       </Head>
